6b1ad98303f8b06ea240fafcd2a992fd1571dfa0
keerthisiv <keerthisiv@gmail.com>
2013-08-23 02:55:22 -0400

    Delete 20130822142221_add_user_id_to_wall_posts.rb
    deleted additional migration files

--- db/migrate/20130822142221_add_user_id_to_wall_posts.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdToWallposts < ActiveRecord::Migration
-  def change
-  	add_column :wallposts, :user_id, :integer
-  	add_index :wallposts, :user_id
-  	remove_column :wallposts, :name
-  end
-end
