bf8d381d293be6a3e0bfebdafb30d76a6c9ce4d3
Keerthi <keerthisiv@gmail.com>
2013-08-23 02:38:51 -0400

    changed the migrations to wallpost table

--- db/migrate/20130822142221_add_user_id_to_wall_posts.rb
+++ db/migrate/20130822142221_add_user_id_to_wall_posts.rb
@@ -1,6 +1,6 @@
-class AddUserIdToWallPosts < ActiveRecord::Migration
+class AddUseridToWallposts < ActiveRecord::Migration
   def change
-  	add_column :Wallposts, :user_id, :integer
+  	add_column :wallposts, :user_id, :integer
   	add_index :wallposts, :user_id
   	remove_column :wallposts, :name
   end
